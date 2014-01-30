9fcd5be22065374b07b0a38cb7b03a38ceb8032b
Chris Mangum <mtdbass5@gmail.com>
2014-01-30 16:36:28 -0500

    got sidebar, info windows, search by name, search / sort by location, and distance labeling working

--- public/js/app2.js
+++ public/js/app2.js
@@ -1,10 +1,13 @@
 
 var app = angular.module('app', []);
 
-app.controller('mainCtrl', ['$scope', '$httpBackend', '_', function ($scope, $httpBackend, _) {
+app.controller('mainCtrl', ['$scope', '$httpBackend', '_', '$sce', function ($scope, $httpBackend, _, $sce) {
   $scope.locations = [];
   $scope.markers = [];
+  $scope.searchMode = false;
+  $scope.sortField = 'name';
 
+  var infoWindow = new google.maps.InfoWindow();
   var map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 5,
     center: new google.maps.LatLng(39.8282, -98.5795),
@@ -18,6 +21,40 @@ app.controller('mainCtrl', ['$scope', '$httpBackend', '_', function ($scope, $ht
     map.fitBounds(bounds);
   }
 
+  function genInfoHtml(loc) {
+    var html = [
+      '<div class="info-window">', 
+      '<div class="basic-info">',
+      '<div class="info-name">'
+    ];
+    if (loc.website !== '-') {
+      if (loc.website.indexOf('http://') === -1) {
+        loc.website = 'http://' + loc.website;
+      }
+      html.push('<a target="_blank" href="', loc.website, '">', loc.name,'</a>');
+    } else {
+      html.push(loc.name);
+    }
+    html.push('</div><div class="info-address1">', loc.address,'</div>',
+        '<div class="info-address2">', loc.city, ', ', loc.state, ' ', loc.zip, '</div>',
+      '</div>',
+      '<div class="row-fluid more-info">',
+        '<div class="span6">',
+          '<div class="filter-info-wrapper">',
+            '<div class="label">Phone</div>',
+            '<div class="filter-info">', loc.phone, '</div>',
+          '</div>',
+        '</div>',
+      '</div>',
+    '</div>');
+    return html.join('');
+  }
+
+  function openInfoWindow() {
+    infoWindow.setContent(genInfoHtml($scope.locations[this.index]));
+    infoWindow.open(map, this);
+  }
+
   function plotShops(data) {
     _.each($scope.locations, function (loc, i) {
       var marker = new google.maps.Marker({
@@ -25,11 +62,59 @@ app.controller('mainCtrl', ['$scope', '$httpBackend', '_', function ($scope, $ht
         position: new google.maps.LatLng(loc.lat, loc.lng),
         index: i
       });
-      //google.maps.event.addListener(marker, 'click', open_info_window);
+      google.maps.event.addListener(marker, 'click', openInfoWindow);
       $scope.markers[i] = marker;
     });
     fitMapBounds();
   }
+  function calcDistances(searchPoint) {
+    _.each($scope.locations, function (loc, i) {
+      var dist = google.maps.geometry.spherical.computeDistanceBetween(
+        searchPoint,
+        new google.maps.LatLng(loc.lat, loc.lng)
+      );
+      dist *= 0.000621371; //convert meters to miles
+      loc.distance = parseFloat(dist.toFixed());
+    });
+  }
+
+  $scope.exitSearch = function () {
+    $scope.searchAddress = '';
+    $scope.searchMode = false;
+    $scope.sortField = 'name';
+  };
+
+  $scope.locationSearch = function () {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': $scope.searchAddress}, function (results, status) {
+      var lat, lng, result;
+      if (results.length) {
+        result = results[0].geometry;
+        map.fitBounds(result.bounds);
+        calcDistances(result.location);
+        $scope.searchMode = true;
+        $scope.sortField = 'distance';
+        $scope.queryAddress = $scope.searchAddress;
+        $scope.$apply();
+      }
+    });
+  };
+
+  $scope.getDistanceLabel = function(locations, i) {
+    var string =  '',
+      distance = _.find([500, 250, 100, 50, 20, 10, 5, 1], function (dist) {
+      return (locations[i].distance >= dist && (i === 0 || locations[i - 1].distance < dist));
+    });
+    if (distance) {
+      string = '<div class="label label-miles">' + distance + '+ Miles</div>';
+    }
+    return $sce.trustAsHtml(string);
+  };
+
+  $scope.activateLocation = function (i) {
+    map.setCenter($scope.markers[i].position);
+    openInfoWindow.call($scope.markers[i]);
+  };
 
   $httpBackend('GET', 'clients.json', null, function (status, data) {
     $scope.locations = angular.fromJson(data);
