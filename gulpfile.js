47c3283f1a4877388d8a849b8d5313d63efa086b
Chris Mangum <mtdbass5@gmail.com>
2014-03-20 15:32:43 -0400

    added compass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,33 @@ var gulp = require('gulp');
 var coffee = require('gulp-coffee');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var compass = require('gulp-compass');
 
 var paths = {
-  scripts: 'public/js/*.coffee'
+  js: 'public/js/*.coffee',
+  scss: 'public/css/scss/*.scss'
 };
 
 gulp.task('scripts', function () {
-  return gulp.src(paths.scripts)
+  return gulp.src(paths.js)
     .pipe(coffee())
     .pipe(uglify())
     .pipe(concat('app.js'))
     .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(paths.scripts, ['scripts'])
+gulp.task('compass', function () {
+  return gulp.src(paths.scss)
+    .pipe(compass({
+      style: 'compressed',
+      sass: 'public/css/scss',
+      css: 'public/css'
+    }));
 });
 
-gulp.task('default', ['scripts', 'watch']);
+gulp.task('watch', function () {
+  gulp.watch(paths.js, ['scripts'])
+  gulp.watch(paths.scss, ['compass'])
+});
+
+gulp.task('default', ['compass', 'scripts', 'watch']);
